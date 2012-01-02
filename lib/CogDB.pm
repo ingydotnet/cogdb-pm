f132a181c663096c00c099b685adecd662d1311d
Ingy döt Net <ingy@ingy.net>
2012-01-01 18:39:20 -0800

    First commit

---
+++ lib/CogDB.pm
@@ -0,0 +1,24 @@
+##
+# name:      CogDB
+# abstract:  A Thoughtful Data Store
+# author:    Ingy döt Net <ingy@cpan.org>
+# license:   perl
+# copyright: 2012
+
+use 5.010;
+use strict;
+use warnings;
+
+package CogDB;
+
+our $VERSION = '0.10';
+
+1;
+
+=head1 SYNOPSIS
+
+...
+
+=head1 DESCRIPTION
+
+...
